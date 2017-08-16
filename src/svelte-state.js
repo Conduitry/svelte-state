7d919b25725ae017da8fe1a47e1e0eeb6fb25596
Conduitry <git@fur.army>
2017-08-16 03:23:32 -0400

    initial version of svelte-state

---
+++ src/svelte-state.js
@@ -0,0 +1,54 @@
+let reducer = state => state
+let listeners = new Set()
+
+let state
+
+export let getState = () => state
+
+export let setReducer = (newReducer, preloadedState = newReducer()) => {
+	reducer = newReducer
+	state = preloadedState
+}
+
+export let dispatch = action => {
+	state = reducer(state, action)
+	for (let listener of listeners) {
+		listener()
+	}
+}
+
+export let subscribe = listener => {
+	listeners.add(listener)
+	return () => {
+		listeners.delete(listener)
+	}
+}
+
+export let combineReducers = reducers => (state, action) => {
+	let newState = {}
+	for (let key in reducers) {
+		newState[key] = reducers[key](state && state[key], action)
+	}
+	return newState
+}
+
+export function connect(component, stateToData) {
+	update(component, stateToData)
+	component.on('destroy', subscribe(() => update(component, stateToData)))
+}
+
+function update(component, stateToData) {
+	let oldData = component.get()
+	let newData = stateToData(state)
+	let changed = false
+	let diff = {}
+	for (let key in newData) {
+		if (oldData[key] !== newData[key]) {
+			changed = true
+			diff[key] = newData[key]
+		}
+	}
+	if (changed) {
+		component.set(diff)
+	}
+}
